d991b159140eb644f1152f3d08be479fe36deca4
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-03-07 09:28:28 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/helto4real/go-ha-client
+
+go 1.22
+
+require (
+	github.com/gorilla/websocket v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    c "github.com/helto4real/go-ha-client"
+)
+
+func main() {
+    client := c.Client{}
+    println(client)
+}
